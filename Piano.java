52d60990db04c86293c29700e029761aa0d60ce9
koendoerman <75791902+koendoerman@users.noreply.github.com>
2021-04-27 08:35:41 -0400

    Made all of the white keys play sound

--- Piano.java
+++ Piano.java
@@ -8,6 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
  */
 public class Piano extends World
 {
+    private String[]whiteKeys=
+    {"a","s","d","f","g","h","j","k","l",";","'","\\"};
+    private String[]whiteNotes=
+    {"3c","3d","3e","3f","3g","3a","3b","4c","4d","4e","4f","4g"};
     /**
      * Make the piano.
      */
@@ -21,7 +25,8 @@ public class Piano extends World
     {
         for (int i=0; i<12; i=i+1)
         {
-            addObject (new Key ("g", "3a.wav"), i*63 + 54, 140);
+            Key key = new Key (whiteKeys[i],  whiteNotes[i] + ".wav", "white-key.png", "white-key-down.png");
+            addObject (key , 54  +  i*63,  140);
         }
     }
 }
